3e71ad967c35abdcf10ea703961c08ac70ef34a2
Ivan Khomenko <khomenko.ivan@gmail.com>
2019-12-15 22:50:42 +0200

    Step 10

--- force-app/main/default/aura/BoatTile/BoatTileController.js
+++ force-app/main/default/aura/BoatTile/BoatTileController.js
@@ -2,13 +2,21 @@
     onBoatClick : function(component, event, helper) {
         var boatSelect = component.getEvent("onBoatSelect");
         var boatSelected = $A.get("e.c:BoatSelected");
+        var plotMapMarker = $A.get("e.c:PlotMapMarker");
         boatSelect.setParams({
             "boatId" : component.get("v.boat").Id
         });
         boatSelected.setParams({
             "boat" : component.get("v.boat")
         });
+        plotMapMarker.setParams({
+            "latitude" : component.get("v.boat").Geolocation__latitude__s,
+            "longitude" : component.get("v.boat").Geolocation__longitude__s,
+            "sObjectId" : boat.Id,
+            "label" : boat.Name 
+        });
         boatSelect.fire();
         boatSelected.fire();
+        plotMapMarker.fire();
     }
 })
